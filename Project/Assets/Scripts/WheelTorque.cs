6647cdacab05b0eae17ef2cb31006e1e2c13c440
Jonathan Bergson <contato@bergson.me>
2022-03-27 16:33:17 -0300

    chore(editorconfig): adicionado editor config

--- Project/Assets/Scripts/WheelTorque.cs
+++ Project/Assets/Scripts/WheelTorque.cs
@@ -5,12 +5,12 @@ public class WheelTorque : MonoBehaviour
     private WheelCollider _wheel;
     public float torque = 1000;
     public float brake = 3000;
-    
+
     private void Start()
     {
         _wheel = GetComponent<WheelCollider>();
     }
-    
+
     private void Update()
     {
         var torqueForce = Input.GetAxis("Vertical") * torque;
